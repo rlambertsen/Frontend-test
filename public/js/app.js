1ea4f54a0bddd5d35b83b373e9330a5679c978a3
Rylan <xxryloxx@gmail.com>
2016-07-20 13:41:39 -0400

    main setup dep installed

---
+++ public/js/app.js
@@ -0,0 +1,1 @@
+angular.module('tester', ['ngMaterial', 'tester.controllers' ])
---
+++ public/js/controllers.js
@@ -0,0 +1,10 @@
+angular.module('tester.controllers', [])
+.controller('AppCtrl', function($scope) {
+  $scope.imagePath = 'img/washedout.png';
+})
+.config(function($mdThemingProvider) {
+  $mdThemingProvider.theme('dark-grey').backgroundPalette('grey').dark();
+  $mdThemingProvider.theme('dark-orange').backgroundPalette('orange').dark();
+  $mdThemingProvider.theme('dark-purple').backgroundPalette('deep-purple').dark();
+  $mdThemingProvider.theme('dark-blue').backgroundPalette('blue').dark();
+});
